99a75f695c6969b239765d4aaf281e0bfe9c467a
NohHaYoung <42965065+NohHaYoung@users.noreply.github.com>
2018-11-01 15:33:26 +0900

    실습1

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 3
+#define COLS 3
+
+	
+void printMatrix(int A[][COLS])
+	{
+
+		int i;
+		int j;
+		
+		for(i=0;i<ROWS;i++){
+			for(j=0;j<COLS;j++)
+				printf("%d ", A[i][j]);
+			printf("\n");
+		}
+	}
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+	{
+		int i;
+		int j;
+		
+		for(i=0;i<ROWS;i++){
+	
+		 for(j=0; j<COLS; j++){
+			C[i][j]=A[i][j]+B[i][j];
+		 }
+		}
+	return;
+	
+	}
+
+int main(void) {
+	
+	int A[ROWS][COLS]={
+		{2, 3, 0},
+		{8, 9, 1},
+		{7, 0, 5}
+	};
+	
+	int B[ROWS][COLS]={
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1}
+	};	
+	
+	int C[ROWS][COLS];
+	
+	addMatrix(A,B,C);
+	printMatrix(C);
+	
+	return 0;
+}
+
